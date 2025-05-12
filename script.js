0cc70e15c9c0cb11c797c93950d8ddcdd624ff76
mimi6210 <mi20060927@icloud.com>
2025-05-12 12:07:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// showMessage関数：ボタンを押すとメッセージを表示する
+function showMessage() {
+    // メッセージボックスを表示
+    alert("BootStrapとjavascriptを使ってみました(ᐡ •̥ ̫ •̥ ᐡ) OKを押してください");
+  }
+  
+// changeBackground関数：ボタンを押すと背景とタイトルの色を変更する
+function changeBackground() {
+  // ページ全体の背景色を、#ffb6c1にする
+  document.body.style.backgroundColor = "#ffb6c1";
+}
+
+function changeBackground() {
+  document.body.classList.add("shimmer");
+}
+
